a89367a5ce201ad07da8ee7ec2039a044cbdad3d
igor-suhorukov <igor.suhorukov@gmail.com>
2018-12-07 10:29:55 +0300

    Resolve classpath on >=JDK9 and old JDK version

--- vicluster-core/src/main/java/org/gridkit/vicluster/telecontrol/Classpath.java
+++ vicluster-core/src/main/java/org/gridkit/vicluster/telecontrol/Classpath.java
@@ -26,7 +26,6 @@ import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.net.URLClassLoader;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
@@ -53,7 +52,7 @@ public class Classpath {
 		List<ClasspathEntry> classpath = CLASSPATH_CACHE.get(classloader);
 		if (classpath == null) {
 			classpath = new ArrayList<Classpath.ClasspathEntry>();
-			fillClasspath(classpath, ClasspathUtils.listCurrentClasspath(((URLClassLoader)classloader)));
+			fillClasspath(classpath, ClasspathUtils.listCurrentClasspath(classloader));
 			classpath = Collections.unmodifiableList(classpath);
 			CLASSPATH_CACHE.put(classloader, classpath);
 		}
@@ -264,7 +263,7 @@ public class Classpath {
 		private boolean lazyJar;
 		private byte[] data;
 		private Map<String, Object> marks;
-		
+
 		/**
 		 * {@link ClasspathEntry} is cached, so it make sense
 		 * to cache various features calculated based on entry
@@ -286,11 +285,11 @@ public class Classpath {
         public synchronized <T> T getMark(String key) {
 		    return (T) (marks == null ? null : marks.get(key));
 		}
-		
+
 		public URL getUrl() {
 			return url;
 		}
-				
+
 		@Override
 		public File getLocalFile() {
 			return file;
@@ -345,8 +344,8 @@ public class Classpath {
 				// do not cache jar content in memory
 				return StreamHelper.readFile(file);
 			}
-		}		
-		
+		}
+
 		public String toString() {
 			return filename;
 		}
